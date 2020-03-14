517714c396ac09b1684a49d84bd583abb06425fc
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-14 07:04:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,10 @@ def key_for_min_value(name_hash)
   name = nil
   smallest_num = nil
   name_hash.each do |key, value|
-    if num < smallest_num
+    if smallest_num == nil || num < smallest_num
+      smallest_num = num
+      smallest_name = name
     end
   end
+  return
 end
