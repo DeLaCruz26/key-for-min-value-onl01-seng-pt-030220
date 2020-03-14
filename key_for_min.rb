e79ee6308275a0f15db90bbcb5c4ff60bdd8633a
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-14 07:12:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest_name = nil
-  smallest_num = nil
+  smallest_name = ""
+  smallest_num = ""
   name_hash.each do |name, num|
-    if smallest_num == nil || num < smallest_num
+    if smallest_num == "" || num < smallest_num
       smallest_num = num
       smallest_name = name
     end
