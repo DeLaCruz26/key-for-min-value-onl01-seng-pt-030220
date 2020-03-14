60a6a8bd2999cade36d5b54a15ef51bfd6e41d02
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-14 06:02:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
    name_hash.each { |key, value|
-      if value <= 1
-        puts :keys
-      else
+      if value 1
         puts :value
+      else
+        puts :keys
       end }
 end
